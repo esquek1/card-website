b310098ca08a7e73d1ca4554452ddb14336c0942
esquek1 <esquejokelly@gmail.com>
2024-11-11 19:42:06 -0800

    Add cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
+import React from 'react';
+
+import { 	useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import card_reena from "./assets/card_reena.svg";
+import card_rizzy from "./assets/card_rizzy.svg";
+import card_leandro from "./assets/card_leandro.svg";
+import card_julienne from "./assets/card_julienne.svg";
+import card_hya from "./assets/card_hya.svg";
+import card_kelly from "./assets/card_kelly.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			{/* <div>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div> */}
+			<div className="card">
+				<div className='card-item'>
+					<img src={card_reena} alt="" />   	
+				</div>
+				<div className='card-item'>
+					<img src={card_rizzy} alt="" />   	
+				</div>
+				<div className='card-item'>
+					<img src={card_leandro} alt="" />   	
+				</div>
+				<div className='card-item'>
+					<img src={card_julienne} alt="" />   	
+				</div>
+				<div className='card-item'>
+					<img src={card_hya} alt="" />   	
+				</div>
+				<div className='card-item'>
+					<img src={card_kelly} alt="" />   	
+				</div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+			</div>
+			<h2>Reena Castillo</h2>
+			<h3>Card Designs</h3>
+			
+			<div>
+				
+			</div>
+			
+		</>
+	)
 }
 
 export default App
