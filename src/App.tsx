2e4f36b627765c63466881e03ffe686fa454df5e
esquek1 <esquejokelly@gmail.com>
2024-11-11 23:05:47 -0800

    Change svg to png

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,37 @@
-import React from 'react';
-
-import { 	useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import card_reena from "./assets/card_reena.svg";
-import card_rizzy from "./assets/card_rizzy.svg";
-import card_leandro from "./assets/card_leandro.svg";
-import card_julienne from "./assets/card_julienne.svg";
-import card_hya from "./assets/card_hya.svg";
-import card_kelly from "./assets/card_kelly.svg";
+import React from "react";
+import "./App.css";
+import card_reena from "./assets/card_reena.png";
+import card_rizzy from "./assets/card_rizzy.png";
+import card_leandro from "./assets/card_leandro.png";
+import card_julienne from "./assets/card_julienne.png";
+import card_hya from "./assets/card_hya.png";
+import card_kelly from "./assets/card_kelly.png";
 
 function App() {
-	return (
-		<>
-			{/* <div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div> */}
-			<div className="card">
-				<div className='card-item'>
-					<img src={card_reena} alt="" />   	
-				</div>
-				<div className='card-item'>
-					<img src={card_rizzy} alt="" />   	
-				</div>
-				<div className='card-item'>
-					<img src={card_leandro} alt="" />   	
-				</div>
-				<div className='card-item'>
-					<img src={card_julienne} alt="" />   	
-				</div>
-				<div className='card-item'>
-					<img src={card_hya} alt="" />   	
-				</div>
-				<div className='card-item'>
-					<img src={card_kelly} alt="" />   	
-				</div>
-
-			</div>
-			<h2>Reena Castillo</h2>
-			<h3>Card Designs</h3>
-			
-			<div>
-				
-			</div>
-			
-		</>
-	)
+    return (
+        <div className="banner">
+            <div className="slider">
+                <div className="card-item">
+                    <img src={card_reena} alt="" />
+                </div>
+                <div className="card-item">
+                    <img src={card_rizzy} alt="" />
+                </div>
+                <div className="card-item">
+                    <img src={card_leandro} alt="" />
+                </div>
+                <div className="card-item">
+                    <img src={card_julienne} alt="" />
+                </div>
+                <div className="card-item">
+                    <img src={card_hya} alt="" />
+                </div>
+                <div className="card-item">
+                    <img src={card_kelly} alt="" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, // This opens the app in the default browser
+	open: true, // This opens the app in the default browser
   },
 })
