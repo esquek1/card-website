136205ce5c4861843ac23b45c6d9b4ba0cb9fe77
esquek1 <esquejokelly@gmail.com>
2024-11-11 20:02:01 -0800

    Add script

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // This opens the app in the default browser
+  },
 })
