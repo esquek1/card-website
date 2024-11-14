959ca7ef2b4d10ace904b178bb4c93827f9058bd
esquek1 <esquejokelly@gmail.com>
2024-11-13 23:19:49 -0800

    Add text

--- src/App.tsx
+++ src/App.tsx
@@ -16,65 +16,108 @@ function App() {
     };
 
     return (
-        <div className="banner">
-            <div className="model">
-                <img src={backgroundImg} />
+        <div className="app">
+            <h1>HELLO! IM:</h1>
+            <h2>REENA CASTILLO</h2>
+            <h3>SENIOR DESIGN STUDENT</h3>
+            <div className="about">
+                I’m a queer Filipino native
+                <br /> to Los Angeles, and
+                <br />
+                illustrations are my jam.
+                <br /> I’m always looking for
+                <br />
+                ways to infuse that
+                <br />
+                creative spark into my
+                <br />
+                work. When I’m not
+                <br />
+                designing, you’ll find me
+                <br />
+                hanging out with animals
+                <br />
+                (specifically my cats),
+                <br />
+                cooking up new recipes,
+                <br />
+                listening to new music,
+                <br />
+                sampling delicious foods,
+                <br />
+                getting competitive in
+                <br />
+                first-person shooter
+                <br />
+                games, or losing myself
+                <br />
+                in a good puzzle. Life’s too
+                <br />
+                short for boring hobbies!
             </div>
-            <div className="container">
-                <div
-                    className="card"
-                    style={{ "--position": -4 } as React.CSSProperties}>
-                    <img src={card_reena} alt="" />
+            <div className="banner">
+                <div className="model">
+                    <img src={backgroundImg} />
                 </div>
-                <div
-                    className="card"
-                    style={{ "--position": -3 } as React.CSSProperties}>
-                    <img src={card_reena} alt="" />
+                <div className="container">
+                    <div className="card-title">
+                        <h1>ILLUSTRATIVE TRADING CARDS!</h1>
+                    </div>
+                    <div
+                        className="card"
+                        style={{ "--position": -4 } as React.CSSProperties}>
+                        <img src={card_reena} alt="" />
+                    </div>
+                    <div
+                        className="card"
+                        style={{ "--position": -3 } as React.CSSProperties}>
+                        <img src={card_reena} alt="" />
+                    </div>
+                    <div
+                        className="card"
+                        style={{ "--position": -2 } as React.CSSProperties}>
+                        <img src={card_leandro} alt="" />
+                    </div>
+                    <div
+                        className="card"
+                        style={{ "--position": -1 } as React.CSSProperties}>
+                        <img src={card_leandro} alt="" />
+                    </div>
+                    <div
+                        className="card"
+                        style={{ "--position": 0 } as React.CSSProperties}>
+                        <img src={card_hya} alt="" />
+                    </div>
+                    <div
+                        className="card"
+                        style={{ "--position": 1 } as React.CSSProperties}>
+                        <img src={card_hya} alt="" />
+                    </div>
+                    <div
+                        className="card"
+                        style={{ "--position": 2 } as React.CSSProperties}>
+                        <img src={card_rizzy} alt="" />
+                    </div>
+                    <div
+                        className="card"
+                        style={{ "--position": 3 } as React.CSSProperties}>
+                        <img src={card_kelly} alt="" />
+                    </div>
+                    <div
+                        className="card"
+                        style={{ "--position": 4 } as React.CSSProperties}>
+                        <img src={card_julienne} alt="" />
+                    </div>
                 </div>
-                <div
-                    className="card"
-                    style={{ "--position": -2 } as React.CSSProperties}>
-                    <img src={card_leandro} alt="" />
-                </div>
-                <div
-                    className="card"
-                    style={{ "--position": -1 } as React.CSSProperties}>
-                    <img src={card_leandro} alt="" />
-                </div>
-                <div
-                    className="card"
-                    style={{ "--position": 0 } as React.CSSProperties}>
-                    <img src={card_hya} alt="" />
-                </div>
-                <div
-                    className="card"
-                    style={{ "--position": 1 } as React.CSSProperties}>
-                    <img src={card_hya} alt="" />
-                </div>
-                <div
-                    className="card"
-                    style={{ "--position": 2 } as React.CSSProperties}>
-                    <img src={card_rizzy} alt="" />
-                </div>
-                <div
-                    className="card"
-                    style={{ "--position": 3 } as React.CSSProperties}>
-                    <img src={card_kelly} alt="" />
-                </div>
-                <div
-                    className="card"
-                    style={{ "--position": 4 } as React.CSSProperties}>
-                    <img src={card_julienne} alt="" />
-                </div>
-            </div>
 
-            <div className="content">
-                <h1>TRADING CARDS </h1>
-                <div className="designer">
-                    <h2>R. CASTILLO</h2>
-                    <p>
-                        <b>Web Design</b>
-                    </p>
+                <div className="content">
+                    <h1>TRADING CARDS </h1>
+                    <div className="designer">
+                        <h2>R. CASTILLO</h2>
+                        <p>
+                            <b>Web Design</b>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
