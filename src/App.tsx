46c3db4a1a2cda7734399eab8209aded0a99b6d1
esquek1 <esquejokelly@gmail.com>
2024-11-14 21:06:48 -0800

    Add about page

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     return (
         <div className="app">
             <Header />
-            {/* <AboutPage /> */}
+            <AboutPage />
         </div>
     );
 }
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,42 +1,55 @@
 import React from "react";
-
+import "../css/About.css";
+import img_polaroid1 from "../assets/polaroids.png";
+import img_polaroid2 from "../assets/polaroid_2.png";
 function about() {
     return (
         <div className="about-container">
-            <div className="about">
-                I’m a queer Filipino native
-                <br /> to Los Angeles, and
-                <br />
-                illustrations are my jam.
-                <br /> I’m always looking for
-                <br />
-                ways to infuse that
-                <br />
-                creative spark into my
-                <br />
-                work. When I’m not
-                <br />
-                designing, you’ll find me
-                <br />
-                hanging out with animals
-                <br />
-                (specifically my cats),
-                <br />
-                cooking up new recipes,
-                <br />
-                listening to new music,
-                <br />
-                sampling delicious foods,
-                <br />
-                getting competitive in
-                <br />
-                first-person shooter
-                <br />
-                games, or losing myself
-                <br />
-                in a good puzzle. Life’s too
-                <br />
-                short for boring hobbies!
+            <div className="about-content">
+                <div className="about-images">
+                    <div className="img_polaroid1">
+                        <img src={img_polaroid1} alt="" />
+                    </div>
+                    <div className="img_polaroid2">
+                        <img src={img_polaroid2} alt="" />
+                    </div>
+                </div>
+
+                <p>
+                    I’m a queer Filipino native
+                    <br /> to Los Angeles, and
+                    <br />
+                    illustrations are my jam.
+                    <br /> I’m always looking for
+                    <br />
+                    ways to infuse that
+                    <br />
+                    creative spark into my
+                    <br />
+                    work. When I’m not
+                    <br />
+                    designing, you’ll find me
+                    <br />
+                    hanging out with animals
+                    <br />
+                    (specifically my cats),
+                    <br />
+                    cooking up new recipes,
+                    <br />
+                    listening to new music,
+                    <br />
+                    sampling delicious foods,
+                    <br />
+                    getting competitive in
+                    <br />
+                    first-person shooter
+                    <br />
+                    games, or losing myself
+                    <br />
+                    in a good puzzle. Life’s too
+                    <br />
+                    short for boring hobbies!
+                </p>
             </div>
         </div>
     );
