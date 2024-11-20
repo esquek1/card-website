dd16c7490e29aad7e5f2dd015ec03ca428e6a421
kellz <esquejokelly@gmail.com>
2024-11-20 14:57:54 -0800

    Change layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,13 @@ import "./App.css";
 
 import Header from "./components/Header.tsx";
 import AboutPage from "./pages/about.tsx";
-
+import Cards from "./pages/card.tsx";
 function App() {
-    const [activeCard, setActiveCard] = useState<number | null>(null);
-    // Function to handle the click and set the active card index
-    const handleClick = (index: number) => {
-        setActiveCard(index);
-    };
-
     return (
         <div className="app">
             <Header />
             <AboutPage />
+            <Cards />
         </div>
     );
 }
---
+++ src/components/Slider.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+type Color = "red" | "yellow" | "orange";
+function Slider() {
+    return <div>Slider</div>;
+}
+
+export default Slider;
